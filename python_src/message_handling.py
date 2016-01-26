768344ce1c5d7baddc8ab8bc1a6520fa486a4b67
Michal Kononenko <michalkononenko@gmail.com>
2016-01-25 21:26:09 -0500

    Worked out simple messaging protocol

--- python_src/message_handling.py
+++ python_src/message_handling.py
@@ -1,9 +1,15 @@
+"""
+Contains a class for creating and recieving serial messages
+"""
 import struct
-import random
-import serial
 import os
+from bitstring import BitArray
 
 SHORT_INT = {'minimum': 0, 'maximum': 65535}
+
+
+class BadPacketError(ValueError):
+    pass
 
 
 class SerialMessage(object):
@@ -11,8 +17,8 @@ class SerialMessage(object):
     _number_of_messages_type = SHORT_INT
     _code_type = SHORT_INT
 
-    _packet_size = 64
-    _checksum_size = 8
+    packet_size = 64
+    checksum_size = 8
 
     def __init__(
             self, packet_number, number_of_packets, instruction_number, data
@@ -27,7 +33,7 @@ class SerialMessage(object):
     @classmethod
     def from_format_dict(cls, info_dict):
         return cls(info_dict['ID'], info_dict['NUMBER_OF_PACKETS'],
-                   info_dict['INSTRUCTION'], info_dict['data'])
+                   info_dict['INSTRUCTION'], info_dict['DATA'])
 
     @property
     def datagram(self):
@@ -54,22 +60,91 @@ class SerialMessage(object):
     @property
     def _checksum(self):
         """
-        Calculates the checksum for a given set of
+        Calculates the checksum for a set of 63 bytes.
         :return:
         """
-        return ''
+        data_to_check = self._header + self.data + self._padding
+        check_bits = ''.join(
+                map(str, map(self._calculate_parity_bit, data_to_check))
+        )
+
+        unchecked_bytes = BitArray(bin=check_bits).tobytes()
+
+        unchecked_bytes_checksum = ''.join(
+                map(str, map(self._calculate_parity_bit, unchecked_bytes))
+        )
+
+        check_byte = BitArray(bin=unchecked_bytes_checksum).tobytes()
+
+        return unchecked_bytes + check_byte
+
+    @staticmethod
+    def _calculate_parity_bit(byte):
+        """
+        Calculate the parity bit for a byte
+        :param bytes byte: The byte for which to calculate the parity bit
+        :return:
+        """
+        return byte % 2
 
     @property
     def _padding(self):
         current_packet_size = \
-            len(self._header) + self._checksum_size + len(self.data)
+            len(self._header) + self.checksum_size + len(self.data)
 
-        required_length = self._packet_size - current_packet_size
+        required_length = self.packet_size - current_packet_size
 
         if len(self._padding_bytes) != required_length:
             self._padding_bytes = os.urandom(required_length)
         return self._padding_bytes
 
-    def send(self, output_serializer=serial.Serial('COM1')):
-        with output_serializer as ser:
-            ser.write(self.datagram)
+    @_padding.setter
+    def _padding(self, padding_to_set):
+        self._padding_bytes = padding_to_set
+
+    @classmethod
+    def from_byte_stream(cls, packet_bytes):
+        if len(packet_bytes) != cls.packet_size:
+            raise BadPacketError(
+                    'The packet to be processed is not %d bytes long',
+                    cls.packet_size
+            )
+
+        parsed_byte_stream = cls.parse_byte_stream(packet_bytes)
+
+        message = cls(parsed_byte_stream['packet_number'],
+                      parsed_byte_stream['number_of_packets'],
+                      1, parsed_byte_stream[
+                          'data'])
+
+        message._padding = parsed_byte_stream['padding']
+
+        if message._checksum != parsed_byte_stream['checksum']:
+            raise BadPacketError('An error was detected in the packet')
+        else:
+            return message
+
+    @classmethod
+    def parse_byte_stream(cls, byte_stream):
+
+        header = byte_stream[0:8]
+        packet_number, number_of_packets, instruction, data_length = \
+            struct.unpack('>HHHH', header)
+
+        data = byte_stream[8:8+data_length]
+
+        padding_length = cls.packet_size - cls.checksum_size - \
+                         data_length - len(header)
+
+        padding = byte_stream[8+data_length:8+data_length+padding_length]
+
+        checksum = byte_stream[8+data_length+padding_length:cls.packet_size]
+
+        return dict(
+                packet_number=packet_number,
+                number_of_packets=number_of_packets,
+                instruction=instruction,
+                data=data,
+                padding=padding,
+                checksum=checksum
+                )
--- test/python/test_message.py
+++ test/python/test_message.py
@@ -2,13 +2,98 @@
 Contains unit tests for :mod:`python_src.message_handler`
 """
 import unittest
-from python_src.message_handling import SerialMessage
+import struct
+from python_src.message_handling import SerialMessage, BadPacketError
 
 __author__ = 'Michal Kononenko'
 
 
 class TestMessage(unittest.TestCase):
     def setUp(self):
-        self.message = SerialMessage(1, 1)
+        self.packet_number = 1
+        self.number_of_packets = 1
+        self.instruction = 1
+        self.data = bytes('foo'.encode('ascii'))
+
+    def _assert_constructor_parameters(self, message):
+        self.assertIsInstance(message, SerialMessage)
+        self.assertEqual(message.packet_number, self.packet_number)
+        self.assertEqual(message.number_of_packets, self.number_of_packets)
+        self.assertEqual(message.data, self.data)
+        self.assertEqual(message.response, [])
 
 
+class TestMessageConstructor(TestMessage):
+    def test_constructor(self):
+        message = SerialMessage(self.packet_number, self.number_of_packets,
+                                self.instruction, self.data
+                                )
+
+        self._assert_constructor_parameters(message)
+
+
+class TestFromFormatDict(TestMessage):
+    def setUp(self):
+        TestMessage.setUp(self)
+        self.format_dict = \
+            {
+                "ID": self.packet_number,
+                "NUMBER_OF_PACKETS": self.number_of_packets,
+                "INSTRUCTION": self.instruction,
+                "DATA": bytes('foo'.encode('ascii'))
+            }
+
+    def test_format_dict_constructor(self):
+        message = SerialMessage.from_format_dict(self.format_dict)
+
+        self._assert_constructor_parameters(message)
+
+
+class TestWithConstructedMessage(TestMessage):
+    """
+    Tests that the datagram length is 64 bytes
+    """
+    def setUp(self):
+        TestMessage.setUp(self)
+        self.expected_datagram_length = 64
+
+        self.message = SerialMessage(
+            self.packet_number, self.number_of_packets, self.instruction,
+            bytes('foo'.encode('ascii'))
+        )
+
+    def test_datagram(self):
+        packet = self.message.datagram
+        self.assertEqual(len(packet), 64)
+
+        self.assertEqual(
+                [self.packet_number, self.number_of_packets,
+                 self.instruction], self._deconstruct_packet(packet))
+
+    @staticmethod
+    def _deconstruct_packet(packet):
+        packet_number = struct.unpack('>H', packet[0:2])
+        number_of_packets = struct.unpack('>H', packet[2:4])
+        instruction = struct.unpack('>H', bytes(packet[4:6]))
+
+        return [x[0] for x in (packet_number, number_of_packets, instruction)]
+
+
+class TestFromByteStream(TestWithConstructedMessage):
+    def setUp(self):
+        TestWithConstructedMessage.setUp(self)
+        self.byte_streamed_message = self.message.datagram
+
+    def test_from_byte_stream(self):
+        message = SerialMessage.from_byte_stream(self.byte_streamed_message)
+        self.assertEqual(message.datagram, self.message.datagram)
+
+    def test_bad_packet_length(self):
+        bad_message = self.byte_streamed_message + b'foo'
+        self.assertNotEqual(len(bad_message), len(self.byte_streamed_message))
+
+        with self.assertRaises(BadPacketError):
+            SerialMessage.from_byte_stream(bad_message)
+
+    def test_bit_flipped_in_message(self):
+        
